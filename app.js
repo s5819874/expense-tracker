dcd4893d9de7fe913d956a524a096202f9a6fd0b
POYI LEE <s5819874@hotmail.com>
2021-03-15 13:56:34 +0800

    feat: add isAuthenticated to show different view templates

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ usePassport(app)
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.user = req.user
+  res.locals.isAuthenticated = req.isAuthenticated()
   next()
 })
 
